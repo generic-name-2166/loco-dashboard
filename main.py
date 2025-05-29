138ddc7a6424a54596738458bee7ec781b9337e8
generic-name-2166 <kirbesson277@mail.ru>
2025-05-29 20:25:14 +0300

    feat: serve flow as ephemeral server

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ def uhh_task() -> None:
     print("side effecting very hard")
 
 
-@flow
+@flow(log_prints=True)
 def main() -> None:
     uhh_task()
     print("Hello from prefect-superset!")
 
 
 if __name__ == "__main__":
-    main()
+    main.serve(
+        name="first-prefect-deployment",
+        cron="* * * * *",
+        tags=["testing"],
+        # kill the deployment on shutdown
+        pause_on_shutdown=False,
+    )
